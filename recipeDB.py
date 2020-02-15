cf26af69c8ea68a5d91eb631a67d9a9cf7b4524f
mseckykoebel <mseckykoebel@gmail.com>
2020-02-15 07:02:28 -0600

    basic DB

---
+++ recipeDB.py
@@ -0,0 +1,12 @@
+{
+    "tools": ["knife"],
+    "methods": [],
+    "preparation": [],
+    "methods": [],
+    "measurements": [],
+    "veg": [],
+    "italian": [],
+    "ingredients": {"protein": [], "fats": [], "carbs": []},
+    "descriptions": {"meat": [], "seafood": [], "vegetables": [], "other": []},
+    "healthyToUnhealthy": {{}, {}, {}},
+}
