753a84f79735a128d31c5798ecce3608a7211815
mseckykoebel <mseckykoebel@gmail.com>
2020-02-15 06:51:14 -0600

    BS and BLK

--- recipe_api.py
+++ recipe_api.py
@@ -13,39 +13,37 @@ def get_page(url):
 
 
 def get_name(url):
-    '''
+    """
     Scrape the given url for the name of the recipe.
 
     Parameters: url (string): Link to an AllRecipes recipe. 
 
     Returns: recipe_name (string): Name of recipe.
-    '''
+    """
     recipe_name = ""
     bs = get_page(url)
-    recipe_name = bs.find(
-        'h1', attrs={'id': 'recipe-main-content'}).contents[0]
+    recipe_name = bs.find("h1", attrs={"id": "recipe-main-content"}).contents[0]
     return recipe_name
 
 
 def get_ingredients(url):
-    '''
+    """
     Scrape the given url for the ingredients of the recipe.
 
     Parameters: url (string): Link to an AllRecipes recipe.
 
     Returns: ingredients (list): List of ingredients from recipe.
-    '''
+    """
     ingredients = []
     bs = get_page(url)
     i = 1
     is_working = True
     while is_working:
         try:
-            list_obj = bs.find(
-                'ul', attrs={'id': 'lst_ingredients_'+str(i)}).contents
+            list_obj = bs.find("ul", attrs={"id": "lst_ingredients_" + str(i)}).contents
             for ing in list_obj:
                 if isinstance(ing, bs4.element.Tag):
-                    ingredients.append(ing.contents[1].attrs['title'])
+                    ingredients.append(ing.contents[1].attrs["title"])
             i += 1
         except:
             is_working = False
@@ -53,13 +51,13 @@ def get_ingredients(url):
 
 
 def get_tools(url):
-    '''
+    """
     Scrape the given url for the name of the recipe.
 
     Parameters: url (string): Link to an AllRecipes recipe.
 
     Returns: tools (list): List of tools used in the recipe.
-    '''
+    """
     tools = []
     return tools
 
@@ -67,13 +65,13 @@ def get_tools(url):
 
 
 def get_methods(url):
-    '''
+    """
     Scrape the given url for the name of the recipe.
 
     Parameters: url (string): Link to an AllRecipes recipe.
 
     Returns: methods (list): List of cooking methods used in the recipe.
-    '''
+    """
     methods = []
     return methods
 
@@ -81,17 +79,18 @@ def get_methods(url):
 
 
 def get_steps(url):
-    '''
+    """
     Scrape the given url for the name of the recipe.
 
     Parameters: url (string): Link to an AllRecipes recipe.
 
     Returns: steps (list): List of steps required to complete the recipe.
-    '''
+    """
     steps = []
     bs = get_page(url)
     list_obj = bs.find(
-        'ol', attrs={'class': 'list-numbers recipe-directions__list'}).contents
+        "ol", attrs={"class": "list-numbers recipe-directions__list"}
+    ).contents
     for step in list_obj:
         if isinstance(step, bs4.element.Tag):
             steps.append(step.contents[1].contents[0].strip())
@@ -104,7 +103,7 @@ def transform_to_veg(recipe_info):
 
 
 def transform_from_veg(recipe_info):
-    return {'test': ['test again!']}
+    return {"test": ["test again!"]}
 
 
 def transform_to_healthy(recipe_info):
