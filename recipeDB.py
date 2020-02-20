3136afa39aa8a9d0b271092803d2ae5b94dffabc
mseckykoebel <mseckykoebel@gmail.com>
2020-02-20 10:38:14 -0600

    adding more to the DB and class

--- recipeDB.py
+++ recipeDB.py
@@ -12,3 +12,22 @@ class RecipeDB:
             # process all of the content in the database
             # for example, set self.tools to be an array of tools
             self.tools = json_data["tools"]
+
+            # set of descriptions
+            self.meat = json_data["descriptions"]["meat"]
+            self.seafood = json_data["descriptions"]["seafood"]
+            self.dessert = json_data["descriptions"]["dessert"]
+            self.veggie = json_data["descriptions"]["veggie"]
+            self.other = json_data["descriptions"]["veggie"]
+
+            # set of seasons
+            self.seasonings = json_data["seasonings"]
+
+            # set of styles
+            self.styles = json_data["styles"]
+
+            # set of measurements
+            self.solidMeasurements = json_data["measurement"]["solids"]
+            self.liquidMeasurements = json_data["measurement"]["liquids"]
+
+            # set of cooking methods
