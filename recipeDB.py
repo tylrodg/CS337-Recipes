613a31fbb8242aba968e098142369b9f24e680fe
mseckykoebel <mseckykoebel@gmail.com>
2020-02-20 16:41:41 -0600

    added some methods that determines if there is meat present

--- recipeDB.py
+++ recipeDB.py
@@ -2,6 +2,7 @@ import json
 import utils
 import random
 
+
 class RecipeDB:
     # initialize the recipeDB.json file
     def __init__(self, path):
@@ -20,6 +21,10 @@ class RecipeDB:
             self.veggie = json_data["descriptions"]["veggie"]
             self.other = json_data["descriptions"]["veggie"]
 
+            # set of preparation
+            self.hardFoodPrep = json_data["preparation"]["hardFoodPrep"]
+            self.regularFoodPrep = json_data["preparation"]["regularFoodPrep"]
+
             # set of seasons
             self.seasonings = json_data["seasonings"]
 
@@ -33,3 +38,32 @@ class RecipeDB:
             # set of cooking methods
             self.primaryMetods = json_data["methods"]["primary"]
             self.secondaryMethods = json_data["methods"]["secondary"]
+
+            # parse and get the healthy to unhealthy transformations
+            # get the structure
+            healthyAndUnhealthy = json_data["healthyToUnhealthy"]
+            self.healthyToUnhealthy = {
+                pairs["healthy"]: pairs["unhealthy"]
+                for pairs in healthyAndUnhealthy
+            }
+            self.unhealthyToHealthy = {
+                pairs["unhealthy"]: pairs["healthy"]
+                for pairs in healthyAndUnhealthy
+            }
+
+    # function that determines if there is something present in some collection
+    def _presentInCollection(self, item, collection):
+        # for all of the elements in the collection
+        for i in collection:
+            if (i in item):
+                return True
+            else:
+                # item was not in collection
+                return False
+
+    def _isIngredientType(self, ingredient, collection):
+        return self._presentInCollection(ingredient.name, collection)
+
+    def _isMeatPresent(self, ingredient):
+        meatIsPresent = self._isIngredientType(ingredient, self.meat)
+        return meatIsPresent
